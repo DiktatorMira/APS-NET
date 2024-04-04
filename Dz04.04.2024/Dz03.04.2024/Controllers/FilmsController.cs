5cbf516d2ba0620e26ae67f270623fdcb7c41400
Diktator <dychervony@gmail.com>
2024-04-04 16:22:37 +0300

    Добавил дз за 04.04.2024

---
+++ Dz04.04.2024/Dz03.04.2024/Controllers/FilmsController.cs
@@ -0,0 +1,32 @@
+﻿using Dz03._04._2024.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Dz03._04._2024.Controllers {
+    public class FilmsController : Controller {
+        FilmsContext db;
+        public FilmsController(FilmsContext db) => this.db = db;
+        public async Task<IActionResult> Index() { return View(await db.films.ToListAsync()); }
+        public async Task<IActionResult> Details(int? id) {
+            if (id == null) return NotFound();
+            var film = await db.films.FirstOrDefaultAsync(f => f.Id == id);
+            if (film == null) return NotFound();
+            return View(film);
+        }
+        public async Task<IActionResult> Delete(int? id) {
+            if (id == null) return NotFound();
+            var film = await db.films.FirstOrDefaultAsync(f => f.Id == id);
+            if (film == null) return NotFound();
+            return View(film);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id) {
+            var film = await db.films.FindAsync(id);
+            if (film != null) db.films.Remove(film);
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Dz04.04.2024/Dz03.04.2024/Models/Films.cs
@@ -0,0 +1,11 @@
+﻿namespace Dz03._04._2024.Models {
+    public class Films {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Director { get; set; }
+        public string? Genre { get; set; }
+        public DateTime Date { get; set; }
+        public string? PosterPath { get; set; }
+        public string? Description { get; set; }
+    }
+}
