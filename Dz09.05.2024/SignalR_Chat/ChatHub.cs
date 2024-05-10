4c99c8fbde9f1841509712e9ea1fd9e8d70eec86
Diktator <dychervony@gmail.com>
2024-05-10 13:17:16 +0300

    Была попытка исправить дз за 09.05.2024. Не особо успешно

--- Dz09.05.2024/SignalR_Chat/ChatHub.cs
+++ Dz09.05.2024/SignalR_Chat/ChatHub.cs
@@ -19,52 +19,29 @@ namespace SignalR_Chat {
                 await db.SaveChangesAsync();
             }
         }
-        public async Task Connect(string userName)
-        {
-            var currentUser = await db.Users
-                .Include(u => u.Messages)
-                .ThenInclude(m => m.User)
-                .FirstOrDefaultAsync(u => u.Name == userName);
-
-            if (currentUser == null)
-            {
-                var newUser = new User
-                {
+        public async Task Connect(string userName) {
+            var currentUser = await db.Users.Include(u => u.Messages).FirstOrDefaultAsync(u => u.Name == userName);
+            if (currentUser == null) {
+                var newUser = new User {
                     ConnectionId = Context.ConnectionId,
                     Name = userName
                 };
                 db.Users.Add(newUser);
             }
-            else
-            {
-                currentUser.ConnectionId = Context.ConnectionId;
-            }
+            else currentUser.ConnectionId = Context.ConnectionId;
             await db.SaveChangesAsync();
 
-            // Отправляем сообщения текущего пользователя в чат
-            if (currentUser != null && currentUser.Messages != null)
-            {
-                foreach (var message in currentUser.Messages)
-                {
-                    await Clients.Caller.SendAsync("AddMessage", message.User?.Name, message.Content);
-                }
-            }
-
-            // Отправляем информацию о подключении нового пользователя и сообщения всех пользователей
-            var allUsers = await db.Users.Include(u => u.Messages).ToListAsync();
-            var messagesData = currentUser?.Messages?.Select(message => new { UserName = message.User?.Name, message.Content }).ToList();
-            await Clients.Caller.SendAsync("AddMessages", messagesData);
-
-            // Отправляем информацию о подключении нового пользователя и сообщения всех пользователей
-            var allUsersData = allUsers.Select(user => new
-            {
+            var allUsers = await db.Users.ToListAsync();
+            var allUsersData = allUsers.Select(user => new {
                 ConnectionId = user.ConnectionId,
                 Name = user.Name,
                 Messages = user.Messages?
                     .Select(message => new { UserName = message.User?.Name, message.Content })
                     .ToList()
             }).ToList();
-
+            foreach (var user in allUsers) {
+                if (user.ConnectionId == Context.ConnectionId) await Clients.Caller.SendAsync("AddMessages", user.Messages);
+            }
             await Clients.Caller.SendAsync("Connected", Context.ConnectionId, userName, allUsersData);
             await Clients.AllExcept(Context.ConnectionId).SendAsync("NewUserConnected", Context.ConnectionId, userName);
         }
